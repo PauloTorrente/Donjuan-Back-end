a043cac0d1070cd59217f069cd5b3b22197c7743
paulotorrente <paulotorrentebr@gmail.com>
2024-09-24 23:23:33 +0200

    Minecraft

--- src/api/auth/auth.service.js
+++ src/api/auth/auth.service.js
@@ -1,8 +1,7 @@
 import User from '../users/users.model.js';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import transporter from '../../config/nodemailer.config.js';
-import crypto from 'crypto'; // For generating password reset token
+import crypto from 'crypto'; // For generating confirmation token
 
 // Function to register a user
 export const register = async (email, password, role) => {
@@ -45,7 +44,7 @@ export const register = async (email, password, role) => {
   console.log('User successfully saved:', savedUser);
 
   // Send confirmation email in Spanish
-  const confirmationUrl = `https://donjuan-rzly.onrender.com/api/users/${confirmationToken}`;
+  const confirmationUrl = `https://donjuan-rzly.onrender.com/api/users/confirm/${confirmationToken}`;
   try {
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
@@ -60,38 +59,4 @@ export const register = async (email, password, role) => {
   }
 
   return savedUser;
-};
-
-// Function to handle forgotten password
-export const forgotPassword = async (email) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error('Usuario no encontrado');
-  }
-
-  // Generate a password reset token
-  const token = crypto.randomBytes(20).toString('hex');
-
-  // Set token expiration to 1 hour
-  user.resetPasswordToken = token;
-  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
-
-  await user.save();
-
-  // Send password reset email in Spanish
-  const resetUrl = `https://donjuan-rzly.onrender.com/reset-password/${token}`;
-
-  try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: user.email,
-      subject: 'Recuperación de Contraseña',
-      text: `Has solicitado una recuperación de contraseña. Por favor, haz clic en el siguiente enlace para restablecer tu contraseña: ${resetUrl}`,
-      html: `<p>Has solicitado una recuperación de contraseña.</p><p>Por favor, haz clic en el siguiente enlace para restablecer tu contraseña:</p><a href="${resetUrl}">${resetUrl}</a>`,
-    });
-    return { message: 'Correo de recuperación de contraseña enviado' };
-  } catch (error) {
-    console.error('Error al enviar el correo de recuperación:', error);
-    throw new Error('Error al enviar el correo de recuperación');
-  }
 };
